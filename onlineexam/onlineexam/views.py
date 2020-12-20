7662aa460b2715e07c7336139eae79a0be8771bf
aditisapt <aditis12399@gmail.com>
2020-12-20 14:30:09 +0530

    code uploaded

--- onlineexam/onlineexam/views.py
+++ onlineexam/onlineexam/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from onlineexam.models import Exam
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def examonline(request):
     return render(request, "base.html")
 
 def quiz(request):
     results=Exam.objects.all()
-    return render(request, "Index.html", {"Exam":results} )
+
+    #paginator=Paginator(results, 1)
+    # page=request.GET.get("page")
+    # try:
+    #     results=paginator.page(page)
+    # except PageNotAnInteger:
+    #     results=paginator.page(1)
+    # except EmptyPage:
+    #     results=paginator.page(paginator.num_pages )
+    return render(request, "Index.html", {"Exam":results})
     
